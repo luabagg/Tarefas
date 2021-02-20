590bda0798c1d8d7d3a5bdb08e4187572fc6915a
luabagg <luanbaggio0@gmail.com>
2021-02-20 07:49:02 -0300

    configurações do DB.

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+require "libs/vendor/autoload.php";
+
+define("DIR", __DIR__);
+define("HOST", "development");
+define("BASE", "tarefas");
+define("USER", "development");
+define("PASS", "development");
